fe21cca8da9bf1866f1a16d6928534098569bb4a
Ganesh Sandeep Kanumilli <89462870+SandeepKanumilli@users.noreply.github.com>
2026-05-12 11:45:21 -0500

    Test : intentional fail for monitor testing

--- tests/math.test.js
+++ tests/math.test.js
@@ -2,7 +2,7 @@ const { add, divide } = require('../src/math');
 
 describe('add()', () => {
   test('adds two positive numbers', () => {
-    expect(add(2, 3)).toBe(5);
+    expect(add(2, 3)).toBe(99);
   });
   test('handles negative numbers', () => {
     expect(add(-1, 1)).toBe(0);
