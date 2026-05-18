36055c531e9e3563f9305e0c91fec74e62b10315
Ganesh Sandeep Kanumilli <89462870+SandeepKanumilli@users.noreply.github.com>
2026-05-18 14:26:42 -0500

    test :  Retrigger 02

--- tests/math.test.js
+++ tests/math.test.js
@@ -2,7 +2,7 @@ const { add, divide } = require('../src/math');
 
 describe('add()', () => {
   test('adds two positive numbers', () => {
-    expect(add(2, 3)).toBe(499);
+    expect(add(2, 3)).toBe(599);
   });
   test('handles negative numbers', () => {
     expect(add(-1, 1)).toBe(0);
